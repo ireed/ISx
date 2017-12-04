d01ff1dff384642c8c833bccec23904b928ff046
ireed <ireed@uiuc.edu>
2017-12-04 10:36:44 -0600

    Decreased max key size for uint32 MPI.

--- MPI/params.h
+++ MPI/params.h
@@ -75,7 +75,8 @@ typedef int KEY_TYPE;
 #ifdef DEBUG
 #define DEFAULT_MAX_KEY (32uLL)
 #elif defined(UINT32_KEYS)
-#define DEFAULT_MAX_KEY (unsigned long long) (UINT_MAX-1)
+//#define DEFAULT_MAX_KEY (unsigned long long) (UINT_MAX-1)
+#define DEFAULT_MAX_KEY (unsigned long long) (1uLL<<30uLL)
 #else
 #define DEFAULT_MAX_KEY (unsigned long long)(1uLL<<28uLL)
 #endif
