62a3d63662dfc28f8abdde77d925fa700bfbc9a6
ireed <ireed@uiuc.edu>
2018-03-20 15:24:03 -0500

    More debug messages

--- MPI/isx.c
+++ MPI/isx.c
@@ -542,7 +542,9 @@ for(KEY_TYPE i=0; i<BUCKET_WIDTH/4; i++) my_local_key_counts[i]=0;
 if(my_rank==0) printf("4 DONE\n");
 for(KEY_TYPE i=BUCKET_WIDTH/4; i<BUCKET_WIDTH/2; i++) my_local_key_counts[i]=0;
 if(my_rank==0) printf("2 DONE\n");
-for(KEY_TYPE i=BUCKET_WIDTH/2; i<BUCKET_WIDTH; i++) my_local_key_counts[i]=0;
+for(KEY_TYPE i=BUCKET_WIDTH/2; i<BUCKET_WIDTH*.75; i++) my_local_key_counts[i]=0;
+if(my_rank==0) printf("1 DONE\n");
+for(KEY_TYPE i=BUCKET_WIDTH*.75; i<BUCKET_WIDTH; i++) my_local_key_counts[i]=0;
 if(my_rank==0) printf("0 DONE\n");
 
   //memset(my_local_key_counts, 0, BUCKET_WIDTH * sizeof(KEY_TYPE));
